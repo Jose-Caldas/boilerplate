d7cc1bec403a7b6391aff99fd285dc2b46953f5f
José Caldas <jrcaldas@yahoo.com.br>
2021-06-16 14:34:05 -0300

    add: component Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,22 @@
+import { Wrapper, Title, Logo, Description, Illustration } from "./styles";
+
 export default function Main() {
   return (
-    <main>
-      <h2>React avançado</h2>
-    </main>
+    <Wrapper>
+      <main>
+        <Logo
+          src="/logo.svg"
+          alt="logo-Atomo e ao lado escrito react avançado"
+        />
+        <Title>React avançado</Title>
+        <Description>
+          TypeScript, ReactJs, NextJs, Styled-Components
+        </Description>
+        <Illustration
+          src="/hero-illustration.svg"
+          alt="figura de um desenvolvedor e uma tela com códigos"
+        />
+      </main>
+    </Wrapper>
   );
 }
