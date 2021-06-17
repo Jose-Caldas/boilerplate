e53d1649793d74e69e177ef42b3b99367d92c321
José Caldas <jrcaldas@yahoo.com.br>
2021-06-17 14:16:12 -0300

    fix: jest and babel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,8 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/icon-512.png" />
+        <link rel="apple-touch-icon" href="/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
